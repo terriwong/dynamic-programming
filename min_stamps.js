a613d42abb6e760aae5307c6b9c6f0492fe95b28
Terri Wong <terriwmj@gmail.com>
2016-03-19 14:49:02 -0700

    take care of edge case 1.

--- min_stamps.js
+++ min_stamps.js
@@ -1,4 +1,16 @@
+// Given: n = total postage needed, stampTypes = an array of stamp values available
+// Supply of each type of stamps is unlimited
+// Write a function that returns the minimum stamps needed at total value n
+// Edge case 1: n = 5, stampTypes = [25] >> best case is use one 25c stamp even if we will waste money
+// Edge case 2: n = 6, stampTypes = [4, 3, 1] >> best case is [3, 3] not [4, 1, 1]
+
+
 function minStampsNeeded(n, stampTypes) {
+
+    // take care of edge case 1
+    if (n < Math.min(stamTypes)) {
+        return 1;
+    } 
     
     // we make an array to hold the minimum possible stamps needed
     // at every total value from 0 to n starting each index with value 0
